a6557db1aa6c090eabcddb88f5b95a3efa65bce4
Erdem <16erdemylmz@gmail.com>
2023-09-26 16:46:59 +0300

    homepage

---
+++ homepage.js
@@ -0,0 +1,32 @@
+const logoBlocks = document.querySelectorAll('.logo-block');
+const logoDiv = document.querySelector('.logo-div');
+
+function initLogo() {
+    logoBlocks.forEach((block, index) => {
+        setTimeout(() => {
+            block.style.filter = "opacity(1)";
+
+            if(index == 0) {
+                block.style.transform = "rotate(30deg)";
+            } else if (index == 1) {
+                block.style.transform = "rotate(-30deg)";
+            } else if (index == 2) {
+                block.style.transform = "rotate(30deg)";
+            } else if (index == 3) {
+                block.style.transform = "rotate(-30deg)";
+
+                setTimeout(() => {
+                    logoDiv.style.animation = "lightbulb 2s";
+
+                    setTimeout(() => {
+                        logoDiv.style.animation = "none";
+                    }, 2000);
+                }, 1000);
+
+            }
+
+        }, 1000 * index)
+    });
+}
+
+initLogo();
